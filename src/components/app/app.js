da1e68ed321a40e107d797c694377630930c84c2
vladimir <lovkiy2012@gmail.com>
2019-12-05 21:53:51 +0300

    1

---
+++ src/components/app/app.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import TodoList from '../todo-list';
+import ItemStatusFilter from '../item-status-filter/';
+
+import './app';
+
+const App = () => {
+
+  const todoData = [
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 }
+  ];
+
+  return (
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
+      <TodoList 
+      todos={todoData}
+      onDeleted={(id)=>{
+        console.log('del',id)
+      }} />
+    </div>
+  );
+};
+export default App
